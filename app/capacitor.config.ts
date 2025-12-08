ae235816e28d4c4bb48bb9dbaa412121d4c577f6
Naresh Kumar D <naresh.day@finforz.com>
2025-12-08 16:03:10 +0530

    feat: initialize Capacitor project with basic React app structure and Android resources.

--- app/capacitor.config.ts
+++ app/capacitor.config.ts
@@ -2,8 +2,24 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.cardpark.app',
-  appName: 'Car Park',
-  webDir: 'dist'
+  appName: 'ParkPro',
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 300,
+      backgroundColor: "#030712",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+  },
 };
 
 export default config;
